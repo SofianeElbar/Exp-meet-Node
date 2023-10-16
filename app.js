188382c43593a1e58dc9bd75e2843e42ad6a7858
SofianeElbar <sofiane.elbar@hotmail.fr>
2023-10-16 14:17:35 +0200

    MVC 16/10/2023

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Project = require("./models/project");
+const projectRoutes = require("./routes/projectRoutes");
 
 // express app
 const app = express();
 
 // connect to MongoDB
 const dbURI =
-  "mongodb+srv://user-portfolio:<password>@cluster0.i2iq53o.mongodb.net/node-portfolio?retryWrites=true&w=majority";
+  "mongodb+srv://user-portfolio:Sloufslouf84@cluster0.i2iq53o.mongodb.net/node-portfolio?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 // register view engine
 app.set("view engine", "ejs");
@@ -79,61 +79,14 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-// Projects routes
-app.get("/projects", (req, res) => {
-  Project.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Projects", projects: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/projects", (req, res) => {
-  const project = new Project(req.body);
-
-  project
-    .save()
-    .then((result) => {
-      res.redirect("/projects");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/project/:id", (req, res) => {
-  const id = req.params.id;
-  Project.findById(id)
-    .then((result) => {
-      res.render("details", { project: result, title: "Project Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/project/:id", (req, res) => {
-  const id = req.params.id;
-  Project.findByIdAndDelete(id)
-    .then((result) => {
-      // on ne peut pas faire une simple redirection sur node lorsqu'on injecte du javascript pour écouter un évènement, il faut envoyer à la place un objet json via API au navigateur avec une propriété de redirection.
-      res.json({ redirect: "/projects" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/projects/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
 //redirects
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
+
+// project routes
+// On scope "/projects" pour ne pas avoir à le rajouter dans projectRoutes.js
+app.use("/projects", projectRoutes);
 
 // 404 page
 app.use((req, res) => {
---
+++ controllers/projectController.js
@@ -0,0 +1,63 @@
+const Project = require("../models/project");
+
+const project_index = (req, res) => {
+  Project.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("projects/index", { title: "All Projects", projects: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const project_details = (req, res) => {
+  const id = req.params.id;
+  Project.findById(id)
+    .then((result) => {
+      res.render("projects/details", {
+        project: result,
+        title: "Project Details",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const project_create_get = (req, res) => {
+  res.render("projects/create", { title: "Create a new blog" });
+};
+
+const project_create_post = (req, res) => {
+  const project = new Project(req.body);
+
+  project
+    .save()
+    .then((result) => {
+      res.redirect("/projects");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const project_delete = (req, res) => {
+  const id = req.params.id;
+  Project.findByIdAndDelete(id)
+    .then((result) => {
+      // on ne peut pas faire une simple redirection sur node lorsqu'on injecte du javascript pour écouter un évènement, il faut envoyer à la place un objet json via API au navigateur avec une propriété de redirection.
+      res.json({ redirect: "/projects" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  project_index,
+  project_details,
+  project_create_get,
+  project_create_post,
+  project_delete,
+};
